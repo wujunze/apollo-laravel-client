b7c8ed7cbca59957f9f4947bbf231254a3bc4c75
wujunze <itwujunze@163.com>
2018-12-24 18:19:10 +0800

    fix config

--- config/apollo.php
+++ config/apollo.php
@@ -2,13 +2,13 @@
 
 return [
     'env_tpl' =>
-"APP_NAME = {'APP_NAME'}
-APP_ENV = {'APP_ENV'}
-APP_KEY = {'APP_KEY'}",
+"APP_NAME = {APP_NAME}
+APP_ENV = {APP_ENV}
+APP_KEY = {APP_KEY}",
     'env_dir' => env('ENV_DIR', app_path()),
     'env_file' => env('ENV_FILE', '.env'),
     'save_dir' => env('SAVE_DIR', storage_path()),
-    'server' => env('APOLLO_SERVER', 'http://47.93.118.85:8080'),
+    'server' => env('APOLLO_SERVER', 'http://127.0.0.1:8080'),
     'app_id' => env('APOLLO_SERVER_APP_ID', 'contract'),
     'namespaces' => env('APOLLO_SERVER_APP_ID', ['application']),
     //灰度发布的客户端ip
