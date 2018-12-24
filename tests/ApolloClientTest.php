2f700b97a3e3da80a18be523779807d9279e557f
wujunze <itwujunze@163.com>
2018-12-24 15:00:06 +0800

    fix test

--- tests/ApolloClientTest.php
+++ tests/ApolloClientTest.php
@@ -3,11 +3,28 @@
 namespace WuJunze\LaravelApollo\Tests;
 
 
+use WuJunze\LaravelApollo\Services\ApolloClientService;
+use WuJunze\LaravelApollo\Services\ApolloClientServiceInterface;
+
 class ApolloClientTest extends TestCase
 {
     public function testDemo()
     {
         $this->assertTrue(true);
+
+    }
+
+    public function testApolloClient()
+    {
+        $server = 'http://localhost:8080';
+
+        $appId = 'contract';
+
+        $namespaces = ['application', 'public.mysql', 'public.redis'];
+
+        $apollo = new ApolloClientService($server, $appId, $namespaces);
+
+        $this->assertInstanceOf(ApolloClientServiceInterface::class, $apollo);
 
     }
 
